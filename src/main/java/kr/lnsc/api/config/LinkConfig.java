350d4ca2e4eadabd84674b50a298aea59f5fb93b
yjlee0235 <yjlee0235@naver.com>
2023-10-19 12:23:54 +0900

    Feat: External Path Generator 관련 설정 추가

--- src/main/java/kr/lnsc/api/config/LinkConfig.java
+++ src/main/java/kr/lnsc/api/config/LinkConfig.java
@@ -1,16 +1,36 @@
 package kr.lnsc.api.config;
 
-import kr.lnsc.api.link.domain.generator.ExpireKeyGenerator;
-import kr.lnsc.api.link.domain.generator.RandomShortenPathGenerator;
-import kr.lnsc.api.link.domain.generator.ShortenPathGenerator;
-import kr.lnsc.api.link.domain.generator.UuidExpireKeyGenerator;
+import kr.lnsc.api.link.domain.generator.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @Configuration
 public class LinkConfig {
 
     @Bean
+    @ConditionalOnProperty(prefix = "path-generator", name = "type", havingValue = "external")
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofMillis(700))
+                .setReadTimeout(Duration.ofMillis(700))
+                .build();
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = "path-generator", name = "type", havingValue = "external")
+    public ShortenPathGenerator externalShortenPathGenerator(@Value("${path-generator.url}") String url, RestTemplate restTemplate) {
+        return new ExternalShortenPathGenerator(url, restTemplate);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
     public ShortenPathGenerator shortenPathGenerator() {
         return new RandomShortenPathGenerator();
     }
